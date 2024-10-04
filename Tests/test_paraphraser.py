f07e999e86d631b50380bd93582762641d345797
Kevin Wang <k499wang@uwaterloo.ca>
2024-10-04 15:00:06 -0400

    On branch paraphrase-tests  Changes to be committed: 	new file:   Tests/test_paraphraser.py 	modified:   main.py 	deleted:    paraphraser.py 	new file:   src/main.py 	new file:   src/paraphraser.py
     Changes not staged for commit:
    	deleted:    Tests/test_paraphraser.py
    	deleted:    main.py
    	modified:   src/main.py
    	modified:   src/paraphraser.py
    
     Untracked files:
    	src/__init__.py
    	src/__pycache__/

---
+++ Tests/test_paraphraser.py
@@ -0,0 +1,4 @@
+from src.paraphraser import paraphrase
+
+def test_paraphrase():
+    result = 
---
+++ src/paraphraser.py
@@ -0,0 +1,24 @@
+import google.generativeai as genai
+import os
+
+try:
+    genai.configure(api_key=os.environ["API_KEY"])
+except KeyError:
+    print("API_KEY not found in environment variables. Please set the API_KEY variable")
+
+
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+chat = model.start_chat(history=[])
+
+
+prompt = "Paraphrase the following paragraph. Give only one response"
+
+def paraphrase(text):
+    response = chat.send_message(prompt+ ": "+ text)
+    print(response.text)
+
+
+
+    
+
